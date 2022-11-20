48fab294c98d026dedd4553ca0266d4b8c528651
Guangxin Yu <1572250276@qq.com>
2022-11-20 11:05:52 +0800

    linked faker and finished the seed

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+# faker
+gem 'faker'
+
 # simple_form
 gem "simple_form", github: "heartcombo/simple_form"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Deleting previous flats...'
+Flat.destroy_all
+puts 'Finished deleting'
+
+puts 'Adding new flats...'
+
+5.times do
+  flat = Flat.new
+  flat.name = Faker::Restaurant.name
+  flat.address = Faker::Address.street_address
+  flat.description = Faker::Restaurant.description
+  flat.price_per_night = Faker::Number.decimal(l_digits: 2)
+  flat.number_of_guests = Faker::Number.non_zero_digit
+  flat.save
+
+  puts "Added #{flat.id} - #{flat.name}"
+end
+
+puts "Finished adding, #{Flat.count} flats have been added."
